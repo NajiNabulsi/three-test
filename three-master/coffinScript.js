70162011894040c472ddfa93e6b765a1c2ec0088
NajiNabulsi <najin546@gmail.com>
2022-04-12 16:44:31 +0200

    coffin

--- three-master/coffinScript.js
+++ three-master/coffinScript.js
@@ -1,3 +1,6 @@
+/**
+* old script for old coffin
+ */
 console.log("coffin Model... start");
 
 // var u = window.location;
@@ -9,11 +12,6 @@ import * as THREE from "./build/three.module.js";
 import { GLTFLoader } from "./examples/jsm/loaders/GLTFLoader.js";
 
 import { OrbitControls } from "./examples/jsm/controls/OrbitControls.js";
-
-/**
- * Background Color
- */
-const whiteBackgroundColor = '#E4CB8F'
 
 /**
  * sizes
@@ -32,14 +30,15 @@ const sizes = {
  const imgUrl = 'http://localhost/3dBlock/wp-content/uploads/2022/04'
 
 //Home url
-// const url =  "http://localhost/code/wp/dynamicList/wp-content/plugins/three-test/three-master";
-// const imgUrl = "http://localhost/code/wp/dynamicList/wp-content/uploads/2022/04";
+// const url =
+//   "http://localhost/code/wp/dynamicList/wp-content/plugins/three-test/three-master";
+// const imgUrl =
+//   "http://localhost/code/wp/dynamicList/wp-content/uploads/2022/04";
 
 /**
  * the model link
  */
 const fairwellkaal = `${url}/puur2.glb`;
-const sunflower = `${url}/sunflower.glb`;
 
 /**
  * Environment map
@@ -80,42 +79,36 @@ const webgl = document.querySelector(".webgl"); // TEST LOADIN
 /**
  * Materials
  */
-//  const grassFhoto = `${imgUrl}/grass.jpeg`;
-//  const grassFhoto = `${imgUrl}/grass-1.jpg`;
-//  const floorTexture = new THREE.TextureLoader().load(grassFhoto);
-// const StandardMaterial = new THREE.MeshStandardMaterial({color: new THREE.Color(0xFDF8EF), roughness : 2  , metalness: 1 });
-const StandardMaterial = new THREE.MeshStandardMaterial({roughness : 2  , metalness: 1 });
+const floorMaterial = new THREE.MeshStandardMaterial();
+floorMaterial.roughness = 0;
+floorMaterial.metalness = 0
 
-// StandardMaterial.roughness = 0.7;
+const modelEnvoMapFhoto = `${imgUrl}/TexturesCom_FabricPlain0052_1_M.jpg`;
+const modelEnvoMapTexture = new THREE.TextureLoader().load(modelEnvoMapFhoto );
+
+const modealMaterial = new THREE.MeshStandardMaterial();
+// modealMaterial.color = new THREE.Color(0xC6BBA7)
+modealMaterial.color = new THREE.Color(0x968766)
+// modealMaterial.roughness =   ;
+modealMaterial.metalness = 1
+modealMaterial.roughness = 1
+modealMaterial.envMap = modelEnvoMapTexture
 
 /**
 * texture
 */
-
-// const matcapFhoto = `${imgUrl}/metacup.jpg`;
+// const matcapFhoto = `${imgUrl}/TexturesCom_FabricPlain0052_1_M.jpg`;
 
 // const sceneEnvironmentFhoto = `${imgUrl}/scene-environment-1.jpg`;
-// // const sceneEnvironmentFhoto = `${imgUrl}/scene-environment.jpg`
-
-// const matcapTexture = new THREE.TextureLoader().load(matcapFhoto);
-// // const sceneEnvironment = new THREE.TextureLoader().load(sceneEnvironmentFhoto);
-
-// const matcapMaterial = new THREE.MeshMatcapMaterial();
-// matcapMaterial.matcap = matcapTexture;
-// // matcapMaterial.roughness = 1
-// // material.color = new THREE.Color('#e0ddd5')
-// matcapMaterial.color = new THREE.Color("#E4CB8F");
+// const sceneEnvironmentFhoto = `${imgUrl}/scene-environment.jpg`
 
 // const matcapTexture = new THREE.TextureLoader().load(matcapFhoto);
 // const sceneEnvironment = new THREE.TextureLoader().load(sceneEnvironmentFhoto);
 
-/** MeshStandardMaterial */
-// const matcapMaterial = new THREE.MeshStandardMaterial();
+// const matcapMaterial = new THREE.MeshMatcapMaterial();
 // matcapMaterial.matcap = matcapTexture;
-// matcapMaterial.roughness = 1
 // material.color = new THREE.Color('#e0ddd5')
 // matcapMaterial.color = new THREE.Color("#E4CB8F");
-//  matcapMaterial.color = new THREE.Color('rgb(105, 45, 14)');
 
 /**
  * Scene
@@ -131,29 +124,25 @@ const scene = new THREE.Scene();
 const loader = new GLTFLoader();
 
 loader.load(
-  // fairwellkaal,
-  sunflower,
+  fairwellkaal,
   gltf => {
     console.log("gltf: ", gltf);
     const model = gltf.scene;
-    model.children[0].traverse(child => {
-      child.castShadow = true;
-      child.receiveShadow = true;
-      
+    model.children[0].traverse(n => {
+      n.castShadow = true;
+      // n.receiveShadow = true;
     });
-    model.children[0].children[0].material.flatShading = true
-   console.log(model.children[0].matrial)
-   console.log( model.children[0].children[0].material.flatShading)
+
     /**
   * to add material for model
   */
-    // model.traverse(child => {
-    //   child.material =  StandardMaterial 
-    // });
+    model.traverse(child => {
+      child.material = modealMaterial;
+    });
     //  model.traverse((child) =>   { child.Mesh.castShadow = true});
-    
-    model.position.set(0, -0.75, 0)
-    model.rotation.set(0, 0, -0.10);
+
+    model.position.y = -1.25;
+    model.rotation.set(0, 0, -0.15);
     scene.add(model);
   },
   /**
@@ -175,75 +164,65 @@ loader.load(
  * Floor ( CircleGeometry )
  */
 
-// const floor = new THREE.Mesh(
-//   new THREE.CircleBufferGeometry(30, 50),
-//   StandardMaterial
-// );
-// floor.rotation.x = -Math.PI * 0.5;
-// floor.position.y = -1.35;
-// floor.receiveShadow = true;
-// scene.add(floor);
+const floor = new THREE.Mesh(
+  new THREE.CircleBufferGeometry(50, 20),
+  floorMaterial
+);
+floor.rotation.x = -Math.PI * 0.5;
+floor.position.y = -1.75;
+floor.receiveShadow = true;
+scene.add(floor);
 
 /**
  * lights
  */
-const dirLight = new THREE.DirectionalLight(0xffffff);
-dirLight.position.set(-0.3, 10, 10 );
-// dirLight.castShadow = true;
-
-// // dirLight.shadow.mapSize.width = 1024; // default
-// // dirLight.shadow.mapSize.height = 1024; // default
-// // dirLight.shadow.camera.near = 0.5; // default
-// // dirLight.shadow.camera.far = 500; // default
+const dirLight = new THREE.PointLight(0xffffff, 1 ,10);
+dirLight.position.set(0, -0.5, 2);
+dirLight.castShadow = true;
 scene.add(dirLight);
 
-// // const helper = new THREE.DirectionalLightHelper( dirLight, 2 );
-// // scene.add( helper );
+// const helper = new THREE.PointLightHelper(dirLight, 1)
+// scene.add(helper)
 
-const dirLight1 = new THREE.DirectionalLight(0xffffff);
-// // dirLight1.castShadow = true
-dirLight1.position.set(15, 8, -10 );
-scene.add(dirLight1);
+const leftLight = new THREE.PointLight(0xffffff, 1 ,10);
+leftLight.position.set(0,-0.5, -2);
+// leftLight.castShadow = true;
+scene.add(leftLight);
 
-// // const helper1 = new THREE.DirectionalLightHelper( dirLight1, 2 );
-// // scene.add( helper1 );
+// const helper1 = new THREE.PointLightHelper(leftLight, 1)
+// scene.add(helper1)
 
-const topLight = new THREE.DirectionalLight(0xd3d2d1)
-topLight.position.set(0, 10, 0)
-// topLight.position.set(-1.5, 0.5, -10 );
-// topLight.castShadow = true
+const forLight = new THREE.PointLight(0xffffff, 1 ,10);
+forLight.position.set(-3, -0.8, 0);
+// forLight.castShadow = true;
+scene.add(forLight);
 
-scene.add(topLight)
+// const helper3 = new THREE.PointLightHelper(forLight, 1)
+// scene.add(helper3)
 
-const frontLight = new THREE.DirectionalLight(0xd3d2d1)
-frontLight.position.set(-10, 0, 0)
-// frontLight.position.set(-1.5, 0.5, -10 );
-// frontLight.castShadow = true
+// const afterLight = new THREE.PointLight(0xffffff, 1 ,10);
+// afterLight.position.set(3, -0.8, 0);
+// // afterLight.castShadow = true;
+// scene.add(afterLight);
 
-scene.add(frontLight)
+// const helper4 = new THREE.PointLightHelper(afterLight, 1)
+// scene.add(helper4)
 
-const buttomLight = new THREE.DirectionalLight(0xd3d2d1)
-buttomLight.position.set(0, -10, 0)
-// buttomLight.position.set(-1.5, 0.5, -10 );
-// buttomLight.castShadow = true
+const topLight = new THREE.PointLight(0xffffff,1,100);
+topLight.position.set(0,1, 0);
+// topLight.castShadow = true;
+// topLight.shadow.mapSize.width = 1024
+// topLight.shadow.mapSize.height = 1024
+scene.add(topLight);
 
-scene.add(buttomLight)
 
-// const topHelper = new THREE.DirectionalLightHelper(topLight, 2)
-// topHelper.castShadow = true
-// scene.add(topHelper)
+// const helper2 = new THREE.PointLightHelper(topLight, 1)
+// scene.add(helper2)
 
-// const pointLight = new THREE.DirectionalLight(0xffffff);
-// pointLight.position.set( 0 , 0, 1.5);
-// pointLight.castShadow = true
-// scene.add( pointLight );
 
-// const sphereSize = 1;
-// const pointLightHelper = new THREE.PointLightHelper( pointLight, sphereSize );
-// scene.add( pointLightHelper );
-
-// const light = new THREE.AmbientLight(0xffffff); // soft white light
+// const light = new THREE.AmbientLight(0x404040); // soft white light
 // scene.add(light);
+
 
 /**
  * camera
@@ -254,7 +233,7 @@ const camera = new THREE.PerspectiveCamera(
 );
 // const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 4;
-camera.position.y = 0;
+camera.position.y = 2;
 camera.position.x = 0;
 scene.add(camera);
 
@@ -265,9 +244,7 @@ scene.add(camera);
 const reSize = 1.5;
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth / reSize, window.innerHeight / reSize);
-// renderer.setClearColor(0xf7f4f4, 1);
-// renderer.setClearColor(0xaaa4a4);
-renderer.setClearColor('#FDF8EF',0.95);
+// renderer.setClearColor(0xfdf8ef, 1);
 renderer.outputEncoding = THREE.sRGBEncoding;
 
 renderer.shadowMap.enabled = true;
@@ -285,8 +262,8 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 // controls.enableZoom = false;
 
-controls.maxPolarAngle = 0;
-controls.minPolarAngle = 1.5;
+// controls.maxPolarAngle = 0;
+// controls.minPolarAngle = 1.5;
 
 controls.autoRotate = true;
 controls.autoRotateSpeed = 1.5;
